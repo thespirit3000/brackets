6d046848f1701b1df738eec2f628f2f11564bb1e
DzianisBogdan <thespirit3000@gmail.com>
2020-03-09 10:49:34 +0300

    feat: brackets task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+const convertedConfig = bracketsConfig => {
+    let convertedArray = [];
+    bracketsConfig.forEach(element => {
+        convertedArray.push(element.join(""));
+    });
+    return convertedArray;
+};
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    const config = convertedConfig(bracketsConfig);
+    const length = config.length;
+    for (let index = 0; index < length; ) {
+        if (str.indexOf(config[index]) != -1) {
+            str = str.replace(config[index], "");
+            index = 0;
+        } else {
+            index += 1;
+        }
+    }
+    return str.length == "";
+};
